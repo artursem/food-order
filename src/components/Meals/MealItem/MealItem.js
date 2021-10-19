d07bc54725dfcf83386aedce50a5a4a297ce1532
artursem <artur.semeniuk@gmail.com>
2021-10-19 18:36:28 +0200

    add ui components

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
 	const price = `$${props.price.toFixed(2)}`;
@@ -12,7 +13,9 @@ const MealItem = (props) => {
 				</div>
 				<div className={classes.price}>{price}</div>
 			</div>
-			<div></div>
+			<div>
+				<MealItemForm/>
+			</div>
 		</li>
 	);
 };
