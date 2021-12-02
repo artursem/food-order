8cbb5f3bd47c7ad003e4cbb32a8c99f47a58ac19
artursem <artur.semeniuk@gmail.com>
2021-12-02 21:03:16 +0100

    submit order to db

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
@@ -8,6 +8,9 @@ import Checkout from "./Checkout";
 
 const Cart = (props) => {
 	const [isCheckout, setIsCheckout] = useState(false);
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [didSubmit, setDidSubmit] = useState(false);
+
 	const cartCtx = useContext(CartContext);
 
 	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -23,6 +26,23 @@ const Cart = (props) => {
 
 	const orderHandler = () => {
 		setIsCheckout(true);
+	};
+
+	const submitOrderHandler = async (userData) => {
+		setIsSubmitting(true);
+		const response = await fetch(
+			"https://react-http-25115-default-rtdb.europe-west1.firebasedatabase.app/orders.json",
+			{
+				method: "POST",
+				body: JSON.stringify({
+					user: userData,
+					odredItems: cartCtx.items,
+				}),
+			}
+		);
+		// add error handling ########################################################
+		setIsSubmitting(false);
+		setDidSubmit(true);
 	};
 
 	const cartItems = (
@@ -46,22 +66,48 @@ const Cart = (props) => {
 				Close
 			</button>
 			{hasItems && (
-				<button className={classes.button} onClick={orderHandler}>
+				<button
+					className={classes.button}
+					onClick={orderHandler}
+					onConfirm={submitOrderHandler}
+				>
 					Order
 				</button>
 			)}
 		</div>
 	);
 
-	return (
-		<Modal onClose={props.onClose}>
+	const cartModalContent = (
+		<React.Fragment>
 			{cartItems}
 			<div className={classes.total}>
 				<span>Total Amount</span>
 				<span>{totalAmount}</span>
 			</div>
-			{isCheckout && <Checkout onCancel={props.onClose} />}
+			{isCheckout && (
+				<Checkout onCancel={props.onClose} onConfirm={submitOrderHandler} />
+			)}
 			{!isCheckout && modalActions}
+		</React.Fragment>
+	);
+
+	const isSubmittingModalContent = <p>Sending order data...</p>;
+	const didSubmitModalContent = (
+		<React.Fragment>
+			<p>Successfully sent the order</p>
+      <div className={classes.actions}>
+			<button className={classes["button--alt"]} onClick={props.onClose}>
+				Close
+			</button>
+		</div>
+		</React.Fragment>
+	);
+
+	return (
+		<Modal onClose={props.onClose}>
+			{!isSubmitting && !didSubmit && cartModalContent}
+			{isSubmitting && isSubmittingModalContent}
+			{didSubmit && didSubmitModalContent}
 		</Modal>
 	);
 };
--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -11,6 +11,8 @@ const Checkout = (props) => {
 		postal: true,
 		city: true,
 	});
+    // add useReducer? ##################################################################
+
 	const nameInputRef = useRef();
 	const streetInputRef = useRef();
 	const postalInputRef = useRef();
@@ -29,12 +31,12 @@ const Checkout = (props) => {
 		const enteredPostalIsValid = isFive(enteredPostal);
 		const enteredCityIsValid = !isEmpty(enteredCity);
 
-        setFormInputsValidity({
-            name: enteredNameIsValid,
-            street: enteredStreetIsValid,
-            postal: enteredPostalIsValid,
-            city: enteredCityIsValid,
-        })
+		setFormInputsValidity({
+			name: enteredNameIsValid,
+			street: enteredStreetIsValid,
+			postal: enteredPostalIsValid,
+			city: enteredCityIsValid,
+		});
 
 		const formIsValid =
 			enteredNameIsValid &&
@@ -46,41 +48,56 @@ const Checkout = (props) => {
 			return;
 		}
 
-		// submit form data
+		props.onConfirm({
+			name: enteredName,
+			street: enteredStreet,
+			postal: enteredPostal,
+			city: enteredCity,
+		});
 	};
 
-    const nameControlClasses = `${classes.control} ${formInputsValidity.name ? '' : classes.invalid}`;
-    const streetControlClasses = `${classes.control} ${formInputsValidity.street ? '' : classes.invalid}`;
-    const postalControlClasses = `${classes.control} ${formInputsValidity.postal ? '' : classes.invalid}`;
-    const cityControlClasses = `${classes.control} ${formInputsValidity.city ? '' : classes.invalid}`;
+	const nameControlClasses = `${classes.control} ${
+		formInputsValidity.name ? "" : classes.invalid
+	}`;
+	const streetControlClasses = `${classes.control} ${
+		formInputsValidity.street ? "" : classes.invalid
+	}`;
+	const postalControlClasses = `${classes.control} ${
+		formInputsValidity.postal ? "" : classes.invalid
+	}`;
+	const cityControlClasses = `${classes.control} ${
+		formInputsValidity.city ? "" : classes.invalid
+	}`;
 
 	return (
 		<form className={classes.form} onSubmit={confirmHandler}>
 			<div className={nameControlClasses}>
 				<label htmlFor="name">Your Name</label>
 				<input type="text" id="name" ref={nameInputRef} />
-                {!formInputsValidity.name && <p>Please enter a valid name</p>}
+				{!formInputsValidity.name && <p>Please enter a valid name</p>}
 			</div>
 			<div className={streetControlClasses}>
 				<label htmlFor="street">Street</label>
 				<input type="text" id="street" ref={streetInputRef} />
-                {!formInputsValidity.street && <p>Please enter a valid street</p>}
+				{!formInputsValidity.street && <p>Please enter a valid street</p>}
 			</div>
 			<div className={postalControlClasses}>
 				<label htmlFor="postal">Postal Code</label>
 				<input type="text" id="postal" ref={postalInputRef} />
-                {!formInputsValidity.postal && <p>Please enter a zip code</p>}
+				{!formInputsValidity.postal && <p>Please enter a zip code</p>}
 			</div>
 			<div className={cityControlClasses}>
 				<label htmlFor="city">City</label>
 				<input type="text" id="city" ref={cityInputRef} />
-                {!formInputsValidity.city && <p>Please enter a city</p>}
+				{!formInputsValidity.city && <p>Please enter a city</p>}
 			</div>
 			<div className={classes.actions}>
 				<button type="button" onClick={props.onCancel}>
 					Cancel
 				</button>
-				<button className={classes.submit}>Confirm</button>
+				<button className={classes.submit} onClick={confirmHandler}>
+					Confirm
+				</button>
 			</div>
 		</form>
 	);
